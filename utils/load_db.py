6c94f212f7a0798786b17f72d306c2ac0c8cf6f3
Victor Huang <vichua2006@gmail.com>
2025-02-08 15:58:55 -0500

    loaded sample data into db

---
+++ utils/load_db.py
@@ -0,0 +1,55 @@
+import sqlite3
+import json
+import datetime
+
+def main():
+    # connect to the database and load json data
+    sqliteConnection = sqlite3.connect("../hackers.db")
+    with open("../data/example_hacker_data.json") as file:
+        hackerData = json.load(file)
+
+    cursor = sqliteConnection.cursor()
+
+    # init hacker table
+    cursor.execute('''CREATE TABLE hackers (
+        hacker_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        badge_code TEXT UNIQUE,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        phone TEXT NOT NULL,
+        updated_at TEXT NOT NULL
+    );
+    ''')
+
+    # init scanned activities table
+    cursor.execute('''CREATE TABLE scans (
+        scan_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        activity_name TEXT NOT NULL,
+        activity_category TEXT NOT NULL,
+        scanned_at TEXT NOT NULL,
+        hacker_badge TEXT NOT NULL,
+        FOREIGN KEY(hacker_badge) REFERENCES hackers(badge_code)
+    );
+    ''')
+
+    for hacker in hackerData:
+        # get the current time in UTC (without +00:00, same format as the provided json)
+        utcTime = datetime.datetime.now(datetime.UTC).isoformat()[:-6]
+        # inserting into the hacker table
+        badge_code = hacker["badge_code"] if hacker["badge_code"] else None # to acccount for hackers who have not checked in; their badge_code will be NULL
+        data = (badge_code, hacker["name"], hacker["email"], hacker["phone"], utcTime)
+        command = '''INSERT INTO hackers (badge_code, name, email, phone, updated_at) VALUES (?, ?, ?, ?, ?);'''
+        cursor.execute(command, data)
+        for activity in hacker["scans"]:
+            # inserting into the scans table
+            activityData = (activity["activity_name"], activity["activity_category"], activity["scanned_at"], hacker["badge_code"])
+            command = '''INSERT INTO scans (activity_name, activity_category, scanned_at, hacker_badge) VALUES (?, ?, ?, ?);'''
+            cursor.execute(command, activityData)
+
+    sqliteConnection.commit()
+    cursor.close()
+    sqliteConnection.close()
+
+
+if __name__ == "__main__":
+    main()
